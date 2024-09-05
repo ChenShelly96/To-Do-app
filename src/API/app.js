7875cd11ea4f6fdc7ff2fc622340aef756550946
ChenShelly <chena1996@gmail.com>
2024-09-05 18:48:33 +0300

    Updates

--- src/API/app.js
+++ src/API/app.js
@@ -8,6 +8,15 @@ const app = express();
 const tasksFilePath = path.join(__dirname, 'tasks.json');
 
 // Middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
