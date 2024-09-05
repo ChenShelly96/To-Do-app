5a7b3c3e8169e7763addc4f57a1c075b5c66da44
ChenShelly <chena1996@gmail.com>
2024-09-05 18:55:12 +0300

    Updates

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -7,7 +7,7 @@ if (window.location.hostname === 'localhost') {
   API_BASE_URL = 'http://localhost:5000/api';
 } else {
 
-  API_BASE_URL = 'https://main--chen-to-do-app.netlify.app/api';
+  API_BASE_URL = 'https://main--chen-to-do-app.netlify.app/';
 }
 
 export default API_BASE_URL;
